4cdae56806283c0ebe49f10aaadaafab8f8f67b1
ArminFH <afierro2021@alu.uct.cl>
2022-09-09 21:10:38 -0400

    FunctioningPOST

--- API_REST/app/Http/Controllers/UserController.php
+++ API_REST/app/Http/Controllers/UserController.php
@@ -1,14 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
-//creado con php artisan make:controller UserController
+
 use Illuminate\Http\Request;
-use App\Models\Student;
+use App\Student;
+
 class UserController extends Controller
 {
-    public function create()
+    public function create(Request $request)
     {
         $students = new Student();
+
         $students->nombre = $request->input('nombre');
         $students->apellido = $request->input('apellido');
 
