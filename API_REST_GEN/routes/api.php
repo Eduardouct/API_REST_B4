d3c2a2c477d89749f2b5a03dabc2a476dce8beeb
ArminFH <afierro2021@alu.uct.cl>
2022-10-17 20:47:31 -0300

    API KEY BASICA - Limpieza

--- API_REST_GEN/routes/api.php
+++ API_REST_GEN/routes/api.php
@@ -13,28 +13,15 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\funcionCrud;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('/usuario', [funcionCrud::class, 'post']);
-Route::get('/usuario', [funcionCrud::class, 'get']);
-Route::get('/get/{tabla}', [funcionCrud::class, 'tablaSelec']);
-Route::get('/usuario/{ID_Usuarios}', [funcionCrud::class, 'getbyid']);
-Route::put('/usuario/put/{ID_Usuarios}', [funcionCrud::class, 'putbyid']);
-Route::delete('/usuario/del/{ID_Usuarios}', [funcionCrud::class, 'deletebyid']);
+
+Route::group(["middleware" => "apikey.valid"], function () {
 
 Route::apiResource('usuarios', UsuariosController::class);
 Route::apiResource('estudiantes', EstudiantesController::class);
 Route::apiResource('ramos', RamosController::class);
 Route::apiResource('pROFESORES', PROFESORESController::class);
+
+});
