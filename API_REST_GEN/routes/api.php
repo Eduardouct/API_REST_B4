8223f46bf90c18e74cd6fa9bfaa1dace7b483d2b
Eduardo <eniripil2017@alu.uct.cl>
2022-10-17 21:46:19 -0300

    delete

--- API_REST_GEN/routes/api.php
+++ API_REST_GEN/routes/api.php
@@ -1,27 +1,5 @@
 <?php
 
-
-use App\Http\Controllers\Api\PROFESORESController;
-
-use App\Http\Controllers\Api\RamosController;
-
-use App\Http\Controllers\Api\EstudiantesController;
-
-use App\Http\Controllers\Api\UsuariosController;
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\funcionCrud;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::group(["middleware" => "apikey.valid"], function () {
-
-Route::apiResource('usuarios', UsuariosController::class);
-Route::apiResource('estudiantes', EstudiantesController::class);
-Route::apiResource('ramos', RamosController::class);
-Route::apiResource('pROFESORES', PROFESORESController::class);
-
-});
